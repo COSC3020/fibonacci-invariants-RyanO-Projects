b1e4616c09a0aeb6673f912fe735ade137179640
Ryan <ryan.opitz@mycc.caspercollege.edu>
2024-11-14 14:59:01 -0700

    Rough Draft
    I don't think the ceiling check is being handled properly but we will see what the tests say.

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
 function fib(n) {
-    return n;
+    // create new array of size n+1
+    var arr = new Array(n+1);
+
+    // end when iteration == n+1
+
+    fibonacci(0, n, arr);
+    return arr;
+}
+
+function fibonacci(i, ceiling, arr)
+{
+    // base cases (i=0) = 0 and (i=1) = 1
+    if( i == 0 )
+    {
+        arr[0] = 0;
+        fibonacci(i+1, ceiling, arr);
+        return;
+    }
+    if( i == 1 && i <= ceiling)
+    {
+        arr[1] = 1;
+        fibonacci(i+1, ceiling, arr);
+        return;
+    }
+
+    // for (n > 1) take prior two elements and store sum in array[i]
+    // No longer base cases, sum prior elements and move to next element.
+    arr[i] = arr[i-1] + arr[i-2];
+
+    //
+    if( i <= ceiling )
+    {
+        fibonacci(i+1, ceiling, arr);
+    }
+
+    return;
 }
